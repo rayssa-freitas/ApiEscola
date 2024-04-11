628a7f9652962ff3b2c1a5500ba5fae5e914394b
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-04-11 19:03:02 -0300

    adiciona codigo inicial

--- src/database/migrations/20240405234234-alterar_tabela_alunos.js
+++ src/database/migrations/20240405234234-alterar_tabela_alunos.js
@@ -3,12 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-
     await queryInterface.addColumn('alunos', 'celular', {
       type: Sequelize.STRING,
       allowNull: true
     });
-
   },
 
   async down (queryInterface, Sequelize) {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express') //Framework da aplicação
 const cors = require('cors') // Biblioteca utilizada para inserir headers http
 const { connection } = require('./database/connection') // Configuração de acesso ao banco de dados
+const routes = require('./routes/routes')
 
 const PORT_API = process.env.PORT_API 
 
@@ -9,7 +10,9 @@ class Server {
   { 
     this.middlewares(server) // Instância do argumento da função para a função middlewares
     this.database()  // Instância da função database
+    server.use(routes)
     this.initializeServer(server) // Instância da função initializeServer
+
   }
 
   async middlewares(app) {
@@ -26,6 +29,7 @@ class Server {
       throw error
     }
   }
+
   async initializeServer(app) {
     // Valor da porta do servidor
     app.listen(PORT_API, () => console.log(`Servidor executando na porta ${PORT_API}`)) // Execução do servidor
